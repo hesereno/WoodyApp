8fe0603a3c051f6ad3ecd937366561a0f8db43f9
hesereno <hesereno@gmail.com>
2017-05-10 19:15:50 +0200

    Login sin acabar - para hacer relacion

--- www/js/app.js
+++ www/js/app.js
@@ -26,15 +26,4 @@ app.controller('AppController', ['$scope', '$http', function($scope, $http){
 
               });
      }
-    $scope.test = function(){
-        $http.get('http://woodyappdev.000webhostapp.com/test.php').
-        then(function(response) {
-            var test = response.data;
-            console.log(test.password)
-            console.log(test.test)
-            console.log(test.username)
-            console.log(test.type)
-
-        });
-    }
 }]);
